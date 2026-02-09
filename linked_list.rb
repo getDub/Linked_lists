243cc30d88b8d992a4929b47abc182166bcfe5c3
getDub <143089345+getDub@users.noreply.github.com>
2026-02-10 06:23:07 +1100

    Insert_at method iserts new nodes with teh given values at the given index. It will also raise and IndexError if the index is a negative or larger than the list size.

--- linked_list.rb
+++ linked_list.rb
@@ -149,30 +149,24 @@ class LinkedList
     # => "( 1 ) -> ( 10 ) -> ( 11 ) -> ( 2 ) -> ( 3 ) -> null"
     # If the method is called with an index that is out of bounds (below 0 or above the list’s size), raise an IndexError.
   def insert_at(index, *values)
-    values.reverse_each do |val|
-      index_counter = 0
-      tmp = @head
-      if @head == nil
-        return nil
-      end
-      
-      if @head != nil && index_counter == index
-        prepend(val)
-        return
-        index_counter += 1
-      end
-      
-      prev = nil
-      cur = @head
+    raise IndexError, "index #{index} is negative" if index < 0 
+    raise IndexError, "index #{index} is out of bounds" if index > size
 
-      while cur != nil && index_counter != index
-        index_counter += 1
-        prev = cur
-        cur = cur.next_node
+    return if values.empty?
+
+    values.reverse_each do |val|
+      if index == 0
+        prepend(val)
+        next
       end
-      if cur != nil 
-        prev.next_node = Node.new(val, cur)
+      
+      tmp = @head
+      (index - 1).times do
+        raise IndexError, "index #{index} is out of bounds" if tmp.nil?
+        tmp = tmp.next_node
       end
+
+      tmp.next_node = Node.new(val, tmp.next_node)
     end
   end
 
@@ -212,8 +206,8 @@ p list
 # # p list.index(100)
 p list.to_s
 # puts list
-# p list.insert_at(3, 5, 6, 10)
-p list.insert_at(0, 99, 999)
+p list.insert_at(3, 5, 6, 10)
+# p list.insert_at(0, 99, 999)
 # p list.insert_at(4, 99, 999 )
 # p list.insert_at(2, 99, 999, 9999)
 puts list
